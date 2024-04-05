9c561c88b7a6e3def224ad87e8ef2913e1bea922
KhurramSagoo <34654560+KhurramSagoo@users.noreply.github.com>
2024-04-05 20:21:09 +0500

    credentials cors

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
-import express from "express"
-import cors from "cors"
-import cookieParser from "cookie-parser"
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
-const app = express()
-app.use(cors({
+const app = express();
+app.use(
+  cors({
     origin: process.env.CORS_ORIGIN,
-    Credential: true
-}))
-app.use(cookieParser())
-app.use(express.json({ limit: "8kb" }))
-app.use(express.urlencoded({ extended: true, limit: "8kb" }))
-app.use(express.static("public"))
-
-
-export { app }
+    // Credential: true,
+    credentials: true,
+  })
+);
+app.use(cookieParser());
+app.use(express.json({ limit: "8kb" }));
+app.use(express.urlencoded({ extended: true, limit: "8kb" }));
+app.use(express.static("public"));
+export { app };
